8842366a4665da7a35759ccf032c0636a6e80e4e
nayan kumar <nayans808@gmail.com>
2025-08-15 23:11:36 +0530

    added the dasboard page

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -4,12 +4,17 @@ import {
   SignOutButton,
   SignedOut,
   UserButton,
+  SignUpButton,
 } from "@clerk/clerk-react";
 
 export const Auth = () => {
   return (
     <div className="">
       <h1>Authentication</h1>
+      <SignInButton mode="modal" />
+      <SignUpButton mode="modal" />
+      <SignOutButton />
+      <UserButton />
     </div>
   );
 };
--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,7 +1,12 @@
+import { useUser } from "@clerk/clerk-react";
+
 export const Dashboard = () => {
+  const { user } = useUser();
   return (
-    <div>
-      <h1>Dasboard</h1>
+    <div className="dashboard-container">
+      <div>
+        <h1>Welcome {user?.firstName}! Here are your Finances</h1>
+      </div>
     </div>
   );
 };
